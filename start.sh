06da0848c493cbeb9b1249c05b5784e05f3b5788
ARUNCO <duncanarani@gmail.com>
2018-10-15 09:52:52 +0300

    secret key

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
-export SECRET_KEY=12345yeyrpl[4]
+export SECRET_KEY=os.environ.get
 export DATABASE_URL='postgresql+psycopg2://dunco:dunco@localhost/pitch3'
 export MAIL_USERNAME=aruncodunco@gmail.com
 export MAIL_PASSWORD=31740141
